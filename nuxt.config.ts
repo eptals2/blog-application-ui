01738ff37ec3c66dfb3ecb1dfd2eeb0a8fbd700e
Ephraim Serrano Talaba <eptals2@gmail.com>
2026-09-05 03:04:39 +0800

    Integrate Tailwind; add API & Auth composables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
+
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000/api'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE
     }
   }
 })
